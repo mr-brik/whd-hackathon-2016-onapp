34f1880fed0d189866c872650a310a1c1d03dd74
Brian Kelly <briankelly@Brians-MBP.whd.local>
2016-03-13 15:30:54 +0100

    Whitespace clean up

--- OnAppManagement/lib/OnAppManagement/Model/OnApp/API/Client.pm
+++ OnAppManagement/lib/OnAppManagement/Model/OnApp/API/Client.pm
@@ -5,7 +5,7 @@ use LWP::UserAgent;
 use Config::JFDI;
 use JSON;
 
-our $DEBUG = 0;
+our $DEBUG       = 0;
 our $CONFIG_PATH = "./etc/on-app.yaml";
 
 use Carp qw/cluck/;
@@ -33,29 +33,32 @@ it under the same terms as Perl itself.
 
 =cut
 
+has 'base_uri' => ( is => 'ro', lazy_build => 1, );
 
-has 'base_uri' => ( is => 'ro', lazy_build => 1,);
 sub _build_base_uri {
-  my ($self)= @_;
+  my ($self) = @_;
   return $self->config->{'base_uri'};
 }
 
-has 'username' => ( is => 'ro', lazy_build => 1,);
+has 'username' => ( is => 'ro', lazy_build => 1, );
+
 sub _build_username {
-  my ($self)= @_;
+  my ($self) = @_;
   return $self->config->{'base_uri'};
 }
 
 has 'password' => ( is => 'ro', lazy_build => 1, );
+
 sub _build_password {
   my ($self) = @_;
-   return $self->config->{'password'};
+  return $self->config->{'password'};
 }
 
 sub config {
-  my ( $self ) = @_;
-  return Config::JFDI->open( $CONFIG_PATH );
+  my ($self) = @_;
+  return Config::JFDI->open($CONFIG_PATH);
 }
+
 =head1 METHODS
 
 =head2 call( $path )
@@ -74,13 +77,21 @@ Where:
 
 sub call {
   my ( $self, $path, $post_param ) = @_;
-  return $post_param ? $self->_post_req( $path, $post_param ) : $self->_get_req( $path );
+  return $post_param
+    ? $self->_post_req( $path, $post_param )
+    : $self->_get_req($path);
 }
 
 sub _get_req {
   my ( $self, $path ) = @_;
-  my $req = HTTP::Request->new( 'GET' => sprintf( '%s/%s.json', $self->base_uri, $path), HTTP::Headers->new( Content_Type => 'application/json', 'Accept' => 'application/json', ) );
-  $req->authorization_basic($self->username, $self->password);
+  my $req = HTTP::Request->new(
+    'GET' => sprintf( '%s/%s.json', $self->base_uri, $path ),
+    HTTP::Headers->new(
+      Content_Type => 'application/json',
+      'Accept'     => 'application/json',
+    )
+  );
+  $req->authorization_basic( $self->username, $self->password );
   my $res = LWP::UserAgent->new->request($req);
   return if not $res->is_success;
   return JSON->new->utf8->decode( $res->decoded_content );
@@ -88,24 +99,29 @@ sub _get_req {
 
 sub _post_req {
   my ( $self, $path, $post_param ) = @_;
-  my $ua = LWP::UserAgent->new();
-  my $content = JSON->new->ascii->encode( $post_param);
-  my $req = HTTP::Request->new(POST => sprintf( '%s/%s.json', $self->base_uri, $path) , HTTP::Headers->new( Content_Type => 'application/json', 'Accept' => 'application/json', ), );
-  $req->authorization_basic($self->username, $self->password);
+  my $ua      = LWP::UserAgent->new();
+  my $content = JSON->new->ascii->encode($post_param);
+  my $req     = HTTP::Request->new(
+    POST => sprintf( '%s/%s.json', $self->base_uri, $path ),
+    HTTP::Headers->new(
+      Content_Type => 'application/json',
+      'Accept'     => 'application/json',
+    ),
+  );
+  $req->authorization_basic( $self->username, $self->password );
   my $res = $ua->request($req);
-  if ( $DEBUG ) {
+  if ($DEBUG) {
     warn $req->content;
     warn $req->as_string;
     warn $res->decoded_content;
   }
   if ( not $res->is_success ) {
-     cluck sprintf('Failed with status line: %s', $res->status_line);
-     return;
+    cluck sprintf( 'Failed with status line: %s', $res->status_line );
+    return;
   }
   return JSON->new->utf8->decode( $res->decoded_content );
 }
 
 __PACKAGE__->meta->make_immutable;
-
 
 1;
